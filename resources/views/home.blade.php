3190e0a5f9356c73bf289a30f0057206308db88b
Guz117 <cristian.guzman117@gmail.com>
2022-02-19 16:37:23 +0100

    minimo di css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,45 @@
 @extends('layout.base')
 
 @section('content')
-
-@foreach ($trains as $train)
-    <p>{{ $train->company }}</p>
-    <p>{{ $train->departur_station }}</p>
-    <p>{{ $train->arival_station }}</p>
-    <p>{{ $train->departures_time }}</p>
-    <p>{{ $train->arrivals_time }}</p>
-    <p>{{ $train->departures_date }}</p>
-    <p>{{ $train->arrivals_date }}</p>
-    <p>{{ $train->code_train }}</p>
-    <p>{{ $train->number_carriages }}</p>
-    <p>
-        @if ($train->on_time == true)
-            Treno in orario
-        @else
-            Treno in ritardo
-        @endif  
-    </p>
-    <p>
-        @if ($train->cancel == true)
-            Cancellato
-        @endif  
-    </p>
-@endforeach
+<div class="trains">
+    <div class="index">
+        <h5>COMPAGNIA</h5>
+        <h5>STAZIONE DI PARTENZA</h5>
+        <h5>STAZIONE DI ARRIVO</h5>
+        <h5>ORARIO DI PARTENZA </h5>
+        <h5>ORARIO DI ARRIVO</h5>
+        <h5>DATA DI PARTENZA</h5>
+        <h5>DATA DI ARRIVO</h5>
+        <h5>CODICE TRENO</h5>
+        <h5>NUMERO CARROZZE</h5>
+        <h5>IN ORARIO</h5>
+        <h5>TRENO SOPPRESSO</h5>
+    </div>
+    @foreach ($trains as $train)
+        <div class="train">
+            <p>{{ $train->company }}</p>
+            <p>{{ $train->departur_station }}</p>
+            <p>{{ $train->arival_station }}</p>
+            <p>{{ $train->departures_time }}</p>
+            <p>{{ $train->arrivals_time }}</p>
+            <p>{{ date_format(date_create($train->departures_date),'d M Y') }}</p>
+            <p>{{ date_format(date_create($train->arrivals_date),'d M Y') }}</p>
+            <p>{{ $train->code_train }}</p>
+            <p>{{ $train->number_carriages }}</p>
+            <p>
+                @if ($train->on_time == true)
+                    Treno in orario
+                @else
+                    Treno in ritardo
+                @endif  
+            </p>
+            <p>
+                @if ($train->cancel == true)
+                    <i class="fa-solid fa-x"></i>
+                @endif  
+            </p>
+        </div>
+    @endforeach
+</div>
 
 @endsection
